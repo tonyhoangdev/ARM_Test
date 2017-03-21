5ea3ee245631f6bb5d84c06ef76bee3880d7afe4
Tony Hoang <hoangtrongminh304@gmail.com>
2017-03-22 00:45:54 +0700

    [MKL46Z] Update device in server

--- MKL46Z_blink/run_Jlink_server.bat
+++ MKL46Z_blink/run_Jlink_server.bat
@@ -1,4 +1,4 @@
 dir /b "C:\Program Files (x86)\SEGGER" > temp
 set /p path= < temp
 del temp
-"C:/Program Files (x86)/SEGGER/%path%/JLinkGDBServerCL.exe" -device S32K144 -if swd -speed 1000
+"C:/Program Files (x86)/SEGGER/%path%/JLinkGDBServerCL.exe" -device MKL46Z256xxx4 -if swd -speed 1000
